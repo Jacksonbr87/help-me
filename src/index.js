85a197b6ce3dc5075b0b834dbeb6ee5a2cb9ba91
Anton Mikhailovskiy <anton.mikhailovskiy.87@gmail.com>
2018-03-15 02:21:52 +0300

    Help me Solution

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ module.exports = function count(s, pairs) {
             // var pairsPow = (pairs[i][0] ** pairs[i][1]);
             var pairsPow = powByMod(pairs[i][0], pairs[i][1], 1000000007);
             n *= pairsPow;
+            n %= 1000000007;
         }
         return n;
     }
@@ -62,6 +63,66 @@ module.exports = function count(s, pairs) {
         return isCoPrimeEuklid(a, b - a);
     }
 
+    function isCoPrimeBinaryShift(a, b) {
+        if (a == 0 || b == 0) {
+            // console.log("break");
+            return false;
+        }
+        var factor = 1;
+        while (true) {
+            if (a == b && a != 1)
+                if (a == 0)
+                    throw 'isCoPrimeBinaryShift(0, 0)'
+                else {
+                    // console.log("1 >> false");
+                    return false;
+                    // return factor * a;
+                }
+            if (a == 0) {
+                if (factor == 1) {
+                    // console.log("2 >> true");
+                    return true;
+                } else {
+                    // console.log("2 >> false");
+                    return false;
+                }
+                // return factor * b;
+            }
+            if (b == 0) {
+                if (factor == 1) {
+                    // console.log("3 >> true");
+                    return true;
+                } else {
+                    // console.log("3 >> false");
+                    return false;
+                }
+                // return factor * a;
+            }
+            if (a == 1 || b == 1) {
+                if (factor == 1) {
+                    // console.log("4 >> true");
+                    return true;
+                } else {
+                    // console.log("4 >> false");
+                    return false;
+                }
+                // return factor;
+            }
+            if (!(a & 1) && !(b & 1)) {
+                factor <<= 1;
+                a >>= 1;
+                b >>= 1;
+            } else if (!(a & 1))
+                a >>= 1;
+            else if (!(b & 1))
+                b >>= 1;
+            else if (b > a)
+                b = (b - a) >> 1;
+            else
+                a = (a - b) >> 1;
+        }
+    }
+
     let sArray = s.split('').reverse();
     let nValue = nResult(pairs);
     let totalTrue = true;
@@ -77,7 +138,8 @@ module.exports = function count(s, pairs) {
         for (let j = 0; j < sArray.length; j++) {
             // console.log("sArray[" + j + "] = " + sArray[j]);
             // let isPrime = isCoPrimeRegular(k + j, nValue);
-            let isPrime = isCoPrimeEuklid(k + j, nValue);
+            // let isPrime = isCoPrimeEuklid(k + j, nValue);
+            let isPrime = isCoPrimeBinaryShift(k + j, nValue);
 
             if (!(sArray[j] == 1 && isPrime === true || sArray[j] == 0 && isPrime === false && k > 0)) {
                 // console.log("break");
